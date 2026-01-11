8c9f4194d17f7297ede67e8e3e7e55085f6ec7f5
humor-404 <bigyamkarmacharya@gmail.com>
2026-01-11 12:49:04 +0545

    feat: image upload added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
@@ -15,21 +16,16 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // serve images
 
 //routes
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/images',require('./routes/images'));
-
-app.get('/',(req,res)=>{
-  res.send({message: 'Hello World!'});
-});
-
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/images', require('./routes/images'));
 
 mongoose
-.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected!"))
-.catch((err)=>console.error(err));
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected!"))
+    .catch((err) => console.error(err));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 })
