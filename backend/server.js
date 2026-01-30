2c8fe3bcb3af644eae9a86add2bda0002d95ccd3
Subodh-Dhamala <subodhdhamala@gmail.com>
2026-01-30 20:51:51 +0545

    mongoDB config moved to config folder

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
@@ -18,11 +17,6 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // serve i
 //routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/images', require('./routes/images'));
-
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected!"))
-    .catch((err) => console.error(err));
 
 const PORT = process.env.PORT || 5000;
 
