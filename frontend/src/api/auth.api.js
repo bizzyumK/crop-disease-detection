c242f63fd9431ec388f3a994714e846a3aefd0c6
humor-404 <bigyamkarmacharya@gmail.com>
2026-02-08 15:20:36 +0545

    reg auth via frontend

--- frontend/src/api/auth.api.js
+++ frontend/src/api/auth.api.js
@@ -3,3 +3,8 @@ import api from './axios';
 export const loginUser = (data) => {
   return api.post('/auth/login', data);
 };
+
+export const registerUser = (data) => {
+  return api.post('/auth/register', data);
+};
+
