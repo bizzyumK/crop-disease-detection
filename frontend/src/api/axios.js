90aefb68a9abc3541c74013b140600a597eb1279
humor-404 <bigyamkarmacharya@gmail.com>
2026-03-01 14:35:47 +0545

    axios.js fixed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,42 @@
-import api from './axios';
+import axios from 'axios';
 
-export const loginUser = async(data) =>{
-  const response = await api.post('/auth/login',data);
-  return response.data;
-}
+const api = axios.create({
+  baseURL : import.meta.env.VITE_API_BASE_URL ||  'http://localhost:5000/api',
 
-export const registerUser = async (data) =>{
-  const response = await api.post('/auth/register',data);
-  return response.data;
-}
+  headers:{
+    "Content-Type": "application/json",
+  },
+
+});
+
+// attach token automatically
+api.interceptors.request.use(
+  (config) =>{
+    const token = localStorage.getItem('token');
+
+    if(token){
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+
+  },
+
+  (error) => Promise.reject(error)
+
+);
+
+
+//auto logout on 401
+api.interceptors.response.use(
+  (response)=>response,
+  (error) =>{
+    if(error.response?.status === 401){
+      localStorage.removeItem('token');
+      window.location.href ='/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
